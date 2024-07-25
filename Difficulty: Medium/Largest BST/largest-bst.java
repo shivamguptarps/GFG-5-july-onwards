b34cccc52e1be8152b978728766f193ea94dacad
Shivam Gupta <132929376+shivamguptarps@users.noreply.github.com>
2024-07-25 22:57:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Largest BST/largest-bst.java
@@ -0,0 +1,153 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+class Node  
+{ 
+    int data; 
+    Node left, right; 
+   
+    public Node(int d)  
+    { 
+        data = d; 
+        left = right = null; 
+    } 
+}
+
+class GFG
+{
+    static Node buildTree(String str)
+    {
+        // Corner Case
+        if(str.length() == 0 || str.equals('N'))
+            return null;
+        String[] s = str.split(" ");
+        
+        Node root = new Node(Integer.parseInt(s[0]));
+        Queue <Node> q = new LinkedList<Node>();
+        q.add(root);
+        
+        // Starting from the second element
+        int i = 1;
+        while(!q.isEmpty() && i < s.length)
+        {
+              // Get and remove the front of the queue
+              Node currNode = q.remove();
+        
+              // Get the curr node's value from the string
+              String currVal = s[i];
+        
+              // If the left child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the left child for the curr node
+                  currNode.left = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.left);
+              }
+        
+              // For the right child
+              i++;
+              if(i >= s.length)
+                  break;
+              currVal = s[i];
+        
+              // If the right child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the right child for the curr node
+                  currNode.right = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.right);
+              }
+              
+              i++;
+        }
+    
+        return root;
+    }
+    
+    public static void main(String args[]) throws IOException {
+    
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t>0)
+        {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            
+            Solution T = new Solution();
+            System.out.println(T.largestBst(root));
+            
+            t--;
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+// class Node  
+// { 
+//     int data; 
+//     Node left, right; 
+   
+//     public Node(int d)  
+//     { 
+//         data = d; 
+//         left = right = null; 
+//     } 
+// }
+
+
+class Solution{
+    
+    static class SubTreeInfo {
+        boolean isBST;
+        int size;
+        int min;
+        int max;
+        
+        SubTreeInfo(boolean isBST, int size, int min, int max) {
+            this.isBST = isBST;
+            this.size = size;
+            this.min = min;
+            this.max = max;
+        }
+    }
+    
+    static int largestBst(Node root) {
+        return largestBstHelper(root).size;
+    }
+        static SubTreeInfo largestBstHelper(Node node) {
+        if (node == null) {
+            return new SubTreeInfo(true, 0, Integer.MAX_VALUE, Integer.MIN_VALUE);
+        }
+        
+        SubTreeInfo leftInfo = largestBstHelper(node.left);
+        SubTreeInfo rightInfo = largestBstHelper(node.right);
+            int min = Math.min(node.data, leftInfo.min);
+            int max = Math.max(node.data, rightInfo.max);
+        
+        if (leftInfo.isBST && rightInfo.isBST && node.data > leftInfo.max && node.data < rightInfo.min) {
+            int size = leftInfo.size + rightInfo.size + 1;
+            return new SubTreeInfo(true, size, min, max);
+        } else {
+            int size = Math.max(leftInfo.size, rightInfo.size);
+            return new SubTreeInfo(false, size, min, max);
+        }
+    }
+    
+}
