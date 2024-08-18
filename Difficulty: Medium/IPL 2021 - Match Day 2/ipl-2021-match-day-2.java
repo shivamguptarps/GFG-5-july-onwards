133aa85efcdab7c12a8cc3b6a791c2719a48b215
Shivam Gupta <132929376+shivamguptarps@users.noreply.github.com>
2024-08-18 17:52:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/IPL 2021 - Match Day 2/ipl-2021-match-day-2.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main {
+    public static void main(String args[]) {
+        // taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // taking total count of testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            // taking total number of elements
+            int n = sc.nextInt();
+
+            // taking size of subArrays
+            int k = sc.nextInt();
+
+            // Declaring and Intializing an array of size n
+            int arr[] = new int[n];
+
+            // adding all the elements to the array
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            // Calling the method max_of_subarrays of class solve
+            // and storing the result in an ArrayList
+            ArrayList<Integer> res = new Solution().max_of_subarrays(arr, n, k);
+
+            // printing the elements of the ArrayList
+            for (int i = 0; i < res.size(); i++)
+                System.out.print(res.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    static ArrayList<Integer> max_of_subarrays(int nums[], int n, int k) {
+        Deque<Integer> dq = new ArrayDeque<>();
+        ArrayList<Integer> arl = new ArrayList<>();
+
+        for(int i=0;i<k;i++){
+            while(!dq.isEmpty() && nums[dq.peekLast()]<=nums[i]) dq.removeLast();
+            dq.add(i);
+        }
+
+        arl.add(nums[dq.peek()]);
+
+        for(int i=k;i<nums.length;i++){
+        if(!dq.isEmpty() && dq.peekFirst()<=i-k) dq.removeFirst();
+        while(!dq.isEmpty() && nums[dq.peekLast()]<=nums[i]) dq.removeLast();
+        dq.add(i);
+        arl.add(nums[dq.peek()]);
+
+        }
+        return arl;
+    }
+}
