9489311bf8d86f66803a6cd9b954d4870d830e2d
Shivam Gupta <132929376+shivamguptarps@users.noreply.github.com>
2025-03-27 20:37:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find length of the longest subarray containing atmost two distinct integers/find-length-of-the-longest-subarray-containing-atmost-two-distinct-integers.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t;
+        t = sc.nextInt();
+        sc.nextLine(); // Consume the newline character
+
+        while (t-- > 0) {
+            String line = sc.nextLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            // Convert ArrayList to array
+            Integer[] arr = new Integer[array.size()];
+            array.toArray(arr);
+
+            Solution ob = new Solution();
+
+            // Call totalFruits method and print result
+            Integer ans = ob.totalElements(arr);
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+        sc.close();
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public static int totalElements(Integer[] arr) {
+        int ans=0;
+        for(int i=0;i<arr.length;i++){
+            Set<Integer> hs = new HashSet<>();
+            int temp=0;
+            for(int j=i;j<arr.length;j++){
+                hs.add(arr[j]);
+                if(hs.size()>2) break;
+                temp++;
+            }
+            ans=Math.max(ans,temp);
+        }
+        return ans;
+    }
+}
