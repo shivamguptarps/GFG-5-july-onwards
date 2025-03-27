b74a869a0a180e06bd513685e00bc1092e6624d9
Shivam Gupta <132929376+shivamguptarps@users.noreply.github.com>
2025-03-27 23:08:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.java
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.java
@@ -1,66 +1,80 @@
 //{ Driver Code Starts
-// Initial Template for Java
 import java.util.*;
-import java.lang.*;
-import java.io.*;
+
+// Driver code
 class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while (T-- > 0) {
-            String[] s = br.readLine().trim().split(" ");
-            int V = Integer.parseInt(s[0]);
-            int E = Integer.parseInt(s[1]);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testcases = sc.nextInt(); // Taking number of test cases as input
+
+        while (testcases-- > 0) {
+            int V = sc.nextInt(); // Number of vertices
+            int E = sc.nextInt(); // Number of edges
+
+            // Initialize adjacency list
             ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
-            for (int i = 0; i < E; i++) {
-                String[] S = br.readLine().trim().split(" ");
-                int u = Integer.parseInt(S[0]);
-                int v = Integer.parseInt(S[1]);
-                adj.get(u).add(v);
-                // adj.get(v).add(u);
+            for (int i = 0; i < V; i++) {
+                adj.add(new ArrayList<>()); // Create a new list for each vertex
             }
+
+            // Add edges to the adjacency list
+            for (int i = 0; i < E; i++) {
+                int u = sc.nextInt();
+                int v = sc.nextInt();
+                adj.get(u).add(v); // Adding edge u -> v
+                adj.get(v).add(u); // Adding edge v -> u (undirected graph)
+            }
+
+            // Create Solution object and call bfsOfGraph
             Solution obj = new Solution();
-            ArrayList<Integer> ans = obj.bfsOfGraph(V, adj);
-            for (int i = 0; i < ans.size(); i++)
-                System.out.print(ans.get(i) + " ");
+            ArrayList<Integer> result = obj.bfsOfGraph(V, adj);
+
+            // Print the result
+            for (int node : result) {
+                System.out.print(node + " ");
+            }
             System.out.println();
         }
+
+        sc.close(); // Close the scanner
     }
 }
 
 // } Driver Code Ends
 
 
+// User function Template for Java
 class Solution {
     // Function to return Breadth First Traversal of given graph.
-        
-        static void bfs(int V, ArrayList<ArrayList<Integer>> adj,ArrayList<Integer> arl,int start,boolean vis[]){
-            Queue<Integer> q = new LinkedList<>();
-            q.add(start);
-            vis[start]=true;
-            while(!q.isEmpty()){
-                int curr=q.remove();
-                arl.add(curr);
-                for(int i=0;i<adj.get(curr).size();i++){
-                    if(!vis[adj.get(curr).get(i)]){
-                    q.add(adj.get(curr).get(i));
-                    vis[adj.get(curr).get(i)]=true;
-                    }
+    void bfs(ArrayList<ArrayList<Integer>> adj ,boolean vis[],int s,ArrayList<Integer> ans){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(s);
+        // System.out.println(q);
+        while(!q.isEmpty()){
+            int p = q.remove();
+            if(!vis[p]){
+
+                ans.add(p);
+                        // System.out.println(ans);
+                
+                vis[p]=true;
+                for(int it : adj.get(p)){
+                    q.add(it);
                 }
             }
-            }
-        
-    
+        }
+    }
     public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
-        ArrayList<Integer> arl = new ArrayList<>();
-        boolean vis[] = new boolean[V];
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        boolean vis[]=new boolean[V];
         // for(int i=0;i<V;i++){
         //     if(!vis[i]){
-                bfs(V,adj,arl,0,vis);
+                bfs(adj,vis,0,ans);
         //     }
         // }
-        return arl;
+        //         System.out.println(ans);
+        // bfs
+
+        return ans;
     }
 }
