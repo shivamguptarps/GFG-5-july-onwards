8a4cc17c85157f389c3d6b77da9cad088146958b
Shivam Gupta <132929376+shivamguptarps@users.noreply.github.com>
2025-08-26 14:29:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Implement Lower Bound/implement-lower-bound.java
+++ Difficulty: Easy/Implement Lower Bound/implement-lower-bound.java
@@ -1,46 +1,17 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine().trim());
-
-        while (tc-- > 0) {
-
-            String[] str = br.readLine().trim().split(" ");
-            int[] a = new int[str.length];
-            for (int i = 0; i < str.length; i++) {
-                a[i] = Integer.parseInt(str[i]);
-            }
-            String[] nk = br.readLine().trim().split(" ");
-            int k = Integer.parseInt(nk[0]);
-            Solution sln = new Solution();
-            int ans = sln.lowerBound(a, k);
-
-            System.out.println(ans);
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
-    int lowerBound(int[] arr, int target) {
-        int low=0,high=arr.length-1;
-        int ans=arr.length;
+    int lowerBound(int[] nums, int x) {
+        int ans=nums.length;
+        int low=0,high=nums.length-1;
+
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(arr[mid]>=target){
+            if(nums[mid]>=x){
                 ans=mid;
                 high=mid-1;
             }
             else low=mid+1;
         }
         return ans;
+        
     }
 }
